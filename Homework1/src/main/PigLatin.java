a294daf16cb71774dfdaccd4f414225edf9f1fd5
aben20807 <aben20807@gmail.com>
2017-03-04 23:47:21 +0800

    Hw1 finished

---
+++ Homework1/src/main/PigLatin.java
@@ -0,0 +1,28 @@
+package main;
+
+public class PigLatin {
+	public static void main(String[] args){
+		for(String arg: args){
+			StringBuilder str = new StringBuilder(arg);//non thread safe
+			if(isVowelAtStart(str)){
+				str = str.append("ay");
+			}
+			else{//append first char to end, append "ay", and delete first char 
+				str = str.append(str.charAt(0)).append("ay").delete(0, 1);
+			}
+			str.setCharAt(0, str.substring(0, 1).toUpperCase().charAt(0));//Upper first char
+			System.out.printf("%s ", str.toString());
+		}
+	}
+	
+	public static boolean isVowelAtStart(StringBuilder input){//check if first char is vowel
+		if(input.charAt(0) == 'a' ||
+				input.charAt(0) == 'e' ||
+				input.charAt(0) == 'i' ||
+				input.charAt(0) == 'o' ||
+				input.charAt(0) == 'u')
+			return true;
+		else
+			return false;
+	}
+}
