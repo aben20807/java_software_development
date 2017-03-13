0199fe7b3f631fa1a93540c37c2bb94c08608ba3
aben20807 <aben20807@gmail.com>
2017-03-13 19:57:14 +0800

    hw2 finished

--- homework2/src/main/RootOfEquation.java
+++ homework2/src/main/RootOfEquation.java
@@ -1,10 +1,22 @@
 package main;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class RootOfEquation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		double a = Double.valueOf(scanner.nextLine());
+		double b = Double.valueOf(scanner.nextLine());
+		double c = Double.valueOf(scanner.nextLine());
+		DecimalFormat numberFormat = new DecimalFormat(scanner.nextLine());
+		
+		double x1 = ((-1*b) + Math.sqrt(b*b - 4*a*c))/(2*a);
+		double x2 = ((-1*b) - Math.sqrt(b*b - 4*a*c))/(2*a);
+		System.out.printf("%s\n%s", numberFormat.format(x1), numberFormat.format(x2));
+		
+		scanner.close();
 	}
 
 }
