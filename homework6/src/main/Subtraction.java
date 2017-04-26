fe4a4c2fe27f0ae20ef9e06cfd6209799de3b083
aben20807 <aben20807@gmail.com>
2017-04-26 09:43:20 +0800

    hw6 can (+) - (-) or (-) - (+)

--- homework6/src/main/Subtraction.java
+++ homework6/src/main/Subtraction.java
@@ -3,6 +3,18 @@ package main;
 public class Subtraction implements IOperation{
 
 	public String perform(String num1, String num2){
+		
+		if(num1.charAt(0) == '-' && num2.charAt(0) != '-'){//(-) - (+)
+			IOperation operation = new Addition();
+			return operation.perform(num1, "-"+num2);
+		}
+		else if(num1.charAt(0) != '-' && num2.charAt(0) == '-'){//(+) - (-)
+			IOperation operation = new Addition();
+			return operation.perform(num1, num2.substring(1));
+		}
+		else{
+			
+		}
 		String ans = num1+"-"+num2;
 		return ans;
 	}
